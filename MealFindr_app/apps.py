6c18eff5814c5d68f87843c6214c804b8c740fdf
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2022-01-28 16:28:41 -0600

    models

---
+++ MealFindr_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MealfindrAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MealFindr_app'
---
+++ MealFindr_app/models.py
@@ -0,0 +1,30 @@
+from logging.config import _LoggerConfiguration
+from unicodedata import name
+from django.db import models
+from django.forms import CharField
+from django.urls import reverse
+
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Eatery(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    givebackService = models.TextField(max_length=50)
+    covidProtocol = models.TextField(max_length=250)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'eatery_id': self.id})
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=500)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
