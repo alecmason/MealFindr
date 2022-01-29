649ffa427c6ecdf8755f252b8e437da76204176c
Luis Chavez <luichav3z@gmail.com>
2022-01-28 19:07:44 -0800

    added index and detail

--- MealFindr_app/views.py
+++ MealFindr_app/views.py
@@ -3,8 +3,18 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from .models import Eatery, Comment
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def eaterys_index(request):
+    eaterys = Eatery.objects.all()
+    return render(request, 'eaterys/index.html', {'eaterys': eaterys})
+
+def eaterys_detail(request, eatery_id):
+    eatery = Eatery.objects.get(id=eatery_id)
+    return render(request, 'eaterys/detail.html', {'eatery' : eatery})
