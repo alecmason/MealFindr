2065ebd35e8c626acf94a143e0abf15444d7fd55
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2022-01-28 18:29:06 -0600

    render home page

--- MealFindr_app/views.py
+++ MealFindr_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return HttpResponse('<h1>The about page</h1>')
