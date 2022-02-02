27b2cc7b847c7dd13d27a4192e22b50b81dae4a3
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2022-02-01 18:21:17 -0600

    profile model

--- MealFindr_app/models.py
+++ MealFindr_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.forms import CharField
 from django.urls import reverse
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -33,3 +36,23 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'eatery_id': self.eatery.id})
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+        primary_key=True,
+        )
+
+    def __str__(self):
+        return self.user
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    print(sender, instance, created)
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+def get_absolute_url(self):
+    return reverse('favorites', kwargs={'profile_id': self.pk})
